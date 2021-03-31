47f83eb9a34fab7d2bf066d9081deec6299e6210
winston11 <winstonmcgriff27@gmail.com>
2021-03-31 10:50:52 -0400

    commit tree 3318a5b9c708d9af620eeb1ce3e4e7bbddbf3c06

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,23 @@
-def clock_angle(time)
-    # code your solution here
+def min (a,b)
+    a < b ? a : b
+  end
+def clock_angle (hour, minute)
+  if hour < 0 or minute < 0
+    puts "Wrong input"
+    return -1
+  end
+  if hour == 12
+    hour = 0
+  end
+  if minute == 60
+    minute = 0
+    hour = hour + 1
+  end
+  hourAngle = 0.5 * (hour * 60 + minute)
+  minuteAngle = minute * 6
+  clockAngle = (hourAngle - minuteAngle).abs
+  return min(360 - clockAngle, clockAngle)
 end
+# puts clockAngle(1, 45).to_s + " degrees"
+# puts clockAngle(3, 30).to_s + " degrees"
+# puts clockAngle(9, 15).to_s + " degrees"
